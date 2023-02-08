ff69b45a0c3c8815ee1150ed4fcdbcd678f2e97d
gmnielsen <nielsen@cheverus.org>
2023-02-08 12:56:33 -0500

    new

--- romi-characterization/src/main/java/frc/robot/Robot.java
+++ romi-characterization/src/main/java/frc/robot/Robot.java
@@ -27,6 +27,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the build.gradle file in the
  * project.
  */
+
 public class Robot extends TimedRobot {
   Joystick m_stick;
 
